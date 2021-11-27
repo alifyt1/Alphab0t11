5fbb8dace8ce1c66821e3cf643c0d5c2613f29ae
alifyt1 <95107384+alifyt1@users.noreply.github.com>
2021-11-27 08:21:43 +0800

    Update donasi.js
    .

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,16 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+return `DONASI BISA / E-MONEY
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0852 4601 0059
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ GADA BWANG
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0823 5006 0897
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama A**F
 `
 	}
 
